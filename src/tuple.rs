f7f590513f7a57adc202d12056bdd731d6d70fb6
Simon Chenery <simon_chenery@yahoo.com>
2023-02-12 21:11:34 +0100

    Make Tuple members private

--- src/tuple.rs
+++ src/tuple.rs
@@ -3,10 +3,10 @@ use std::fmt;
 #[derive(Copy, Clone)]
 pub struct Tuple
 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub w: f64,
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
 }
 
 impl Tuple
