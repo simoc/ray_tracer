d4defa97719959d686761414755c558f05192db7
Simon Chenery <simon_chenery@yahoo.com>
2023-02-17 20:27:40 +0100

    Add test for writing a pixel to canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -17,17 +17,23 @@ impl Canvas
         }
         self.pixels[y][x] = c;
     }
+
+    pub fn pixel_at(self, x : usize, y: usize) -> Tuple
+    {
+        return self.pixels[y][x];
+    }
 }
 
 pub fn create_canvas(width: usize, height: usize) -> Canvas
 {
+    let color_black = create_color(0.0, 0.0, 0.0);
     let mut pixels = Vec::with_capacity(height);
     for _ in 0..height
     {
         let mut row = Vec::with_capacity(width);
         for _ in 0..width
         {
-            row.push(create_color(0.0, 0.0, 0.0));
+            row.push(color_black);
         }
         pixels.push(row);
     }
@@ -50,8 +56,14 @@ mod tests
         {
             for x in 0..c1.width
             {
-                assert!(equal(c1.pixels[y][x], create_color(0.0, 0.0, 0.0)));
+                assert!(equal(c1.pixel_at(x, y), create_color(0.0, 0.0, 0.0)));
             }
         }
+
+        // p.19 Scenario: Writing pixels to a canvas
+        let c2 = create_canvas(10, 20);
+        let color_red = create_color(1.0, 0.0, 0.0);
+        c2.write_pixel(2, 3, color_red);
+        assert!(equal(c2.pixel_at(2, 3), color_red));
     }
 }
