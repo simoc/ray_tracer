a1e2cac1142d67535894b6e4cefc58194254e9d2
Simon Chenery <simon_chenery@yahoo.com>
2023-02-24 19:17:44 +0100

    Add tests for matrix equality

--- src/matrix.rs
+++ src/matrix.rs
@@ -35,7 +35,7 @@ impl fmt::Display for Matrix
     }
 }
 
-pub fn create_matrix(rows: usize, columns: usize, cell_values: Vec<f64>) -> Matrix
+pub fn create_matrix(rows: usize, columns: usize, cell_values: &Vec<f64>) -> Matrix
 {
     let mut cell_index = 0;
     let mut cells = Vec::with_capacity(rows);
@@ -59,6 +59,43 @@ pub fn create_matrix(rows: usize, columns: usize, cell_values: Vec<f64>) -> Matr
     Matrix{rows: rows, columns: columns, cells: cells}
 }
 
+pub fn from(cell_values: &str) -> Matrix
+{
+    let mut cells = Vec::new();
+    let mut columns = 0;
+    let without_separators = cell_values.replace("|", " ");
+    let lines = without_separators.lines();
+    for line in lines
+    {
+        let mut row = Vec::new();
+        for n in line.split_whitespace()
+        {
+            row.push(n.parse::<f64>().unwrap());
+        }
+        columns = row.len();
+    }
+    Matrix{rows: cells.len(), columns: columns, cells: cells}
+}
+
+pub fn equal(a: Matrix, b: Matrix) -> bool
+{
+    if (a.rows != b.rows || a.columns != b.columns)
+    {
+        return false;
+    }
+    for y in 0..a.rows
+    {
+        for x in 0..a.columns
+        {
+            if (!fuzzy_equal(a.cells[y][x], b.cells[y][x]))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 #[cfg(test)]
 mod tests
 {
@@ -68,7 +105,7 @@ mod tests
     fn test_matrices_feature()
     {
         // p.26 Scenario: Constructing and inspecting a 4x4 matrix
-        let m1 = create_matrix(4, 4, vec![1.0, 2.0, 3.0, 4.0,
+        let m1 = create_matrix(4, 4, &vec![1.0, 2.0, 3.0, 4.0,
             5.5, 6.5, 7.5, 8.5,
             9.0, 10.0, 11.0, 12.0,
             13.5, 14.5, 15.5, 16.5]);
@@ -82,7 +119,7 @@ mod tests
         assert!(fuzzy_equal(m1.at(3, 2), 15.5));
 
         // p.27 Scenario: A 2x2 matrix ought to be representable
-        let m2 = create_matrix(2, 2, vec![-3.0, 5.0,
+        let m2 = create_matrix(2, 2, &vec![-3.0, 5.0,
             1.0, -2.0]);
         assert!(fuzzy_equal(m2.at(0, 0), -3.0));
         assert!(fuzzy_equal(m2.at(0, 1), 5.0));
@@ -90,11 +127,29 @@ mod tests
         assert!(fuzzy_equal(m2.at(1, 1), -2.0));
 
         // p.27 Scenario: A 3x3 matrix ought to be representable
-        let m3 = create_matrix(3, 3, vec![-3.0, 5.0, 0.0,
+        let m3 = create_matrix(3, 3, &vec![-3.0, 5.0, 0.0,
             1.0, -2.0, -7.0,
             0.0, 1.0, 1.0]);
         assert!(fuzzy_equal(m3.at(0, 0), -3.0));
         assert!(fuzzy_equal(m3.at(1, 1), -2.0));
         assert!(fuzzy_equal(m3.at(2, 2), 1.0));
+
+        // p.27 Scenario: Matrix equality with identical matrices
+        let v4 = vec![1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 8.0, 7.0, 6.0,
+            5.0, 4.0, 3.0, 2.0];
+        let m4 = create_matrix(4, 4, &v4);
+        let m5 = create_matrix(4, 4, &v4);
+        assert!(equal(m4, m5));
+
+        // p.27 Scenario: Matrix equality with different matrices
+        let m6 = create_matrix(4, 4, &v4);
+        let v7 = vec![2.0, 3.0, 4.0, 5.0,
+            6.0, 7.0, 8.0, 9.0,
+            8.0, 7.0, 6.0, 5.0,
+            4.0, 3.0, 2.0, 1.0];
+        let m7 = create_matrix(4, 4, &v7);
+        assert!(!equal(m6, m7));
     }
 }
