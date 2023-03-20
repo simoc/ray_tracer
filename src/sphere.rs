84ee36f913d39b9875f92b9f8345da13bf1e24d2
Simon Chenery <simon_chenery@yahoo.com>
2023-03-20 22:36:00 +0100

    Add Sphere.intersect tests

--- src/sphere.rs
+++ src/sphere.rs
@@ -70,9 +70,39 @@ mod tests
         // p.59 Scenario: A ray intersects a sphere at two points
         let r3 = Ray::new(create_point(0.0, 0.0, -5.0), create_vector(0.0, 0.0, 1.0));
         let s3 = Sphere::new(3);
-        let xs = s3.intersect(r3);
-        assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], 4.0);
-        assert_eq!(xs[1], 6.0);
+        let xs3 = s3.intersect(r3);
+        assert_eq!(xs3.len(), 2);
+        assert_eq!(xs3[0], 4.0);
+        assert_eq!(xs3[1], 6.0);
+
+        // p.60 Scenario: A ray intersects a sphere at a tangent
+        let r4 = Ray::new(create_point(0.0, 1.0, -5.0), create_vector(0.0, 0.0, 1.0));
+        let s4 = Sphere::new(4);
+        let xs4 = s4.intersect(r4);
+        assert_eq!(xs4.len(), 2);
+        assert_eq!(xs4[0], 5.0);
+        assert_eq!(xs4[1], 5.0);
+
+        // p.60 Scenario: A ray misses a sphere
+        let r5 = Ray::new(create_point(0.0, 2.0, -5.0), create_vector(0.0, 0.0, 1.0));
+        let s5 = Sphere::new(5);
+        let xs5 = s5.intersect(r5);
+        assert_eq!(xs5.len(), 0);
+
+        // p.61 Scenario: A ray originates inside a sphere
+        let r6 = Ray::new(create_point(0.0, 0.0, 0.0), create_vector(0.0, 0.0, 1.0));
+        let s6 = Sphere::new(6);
+        let xs6 = s6.intersect(r6);
+        assert_eq!(xs6.len(), 2);
+        assert_eq!(xs6[0], -1.0);
+        assert_eq!(xs6[1], 1.0);
+
+        // p.62 Scenario: A sphere is behind a ray
+        let r7 = Ray::new(create_point(0.0, 0.0, 5.0), create_vector(0.0, 0.0, 1.0));
+        let s7 = Sphere::new(7);
+        let xs7 = s7.intersect(r7);
+        assert_eq!(xs7.len(), 2);
+        assert_eq!(xs7[0], -6.0);
+        assert_eq!(xs7[1], -4.0);
     }
 }
