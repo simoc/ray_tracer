6c711c53c9145b10c254a5c975047096bc08d70c
Simon Chenery <simon_chenery@yahoo.com>
2023-03-20 21:24:35 +0100

    Add Sphere.intersect

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use crate::tuple::*;
+use crate::ray::*;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Sphere
@@ -11,7 +13,29 @@ impl Sphere
     pub fn new(id: i32) -> Self
     {
         Sphere{id: id}
-	}
+    }
+
+    pub fn intersect(&self, ray: Ray) -> Vec<f64>
+    {
+        // the vector from the sphere's centre, to the ray origin
+        // remember: the sphere is centred at the world origin
+        let sphere_to_ray = ray.origin.sub(create_point(0.0, 0.0, 0.0));
+
+        let a = ray.direction.dot_product(ray.direction);
+        let b = 2.0 * sphere_to_ray.dot_product(ray.direction);
+        let c = sphere_to_ray.dot_product(sphere_to_ray) - 1.0;
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        if discriminant < 0.0
+        {
+            return Vec::new();
+        }
+
+        let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
+        let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+        return vec![t1, t2];
+    }
 }
 
 impl PartialEq for Sphere
@@ -19,7 +43,7 @@ impl PartialEq for Sphere
     fn eq(&self, other: &Self) -> bool
     {
         self.id == other.id
-	}
+    }
 }
 
 impl fmt::Display for Sphere
@@ -42,5 +66,13 @@ mod tests
         let s2 = Sphere::new(2);
         assert_eq!(s1, s1);
         assert_ne!(s1, s2);
+
+        // p.59 Scenario: A ray intersects a sphere at two points
+        let r3 = Ray::new(create_point(0.0, 0.0, -5.0), create_vector(0.0, 0.0, 1.0));
+        let s3 = Sphere::new(3);
+        let xs = s3.intersect(r3);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], 4.0);
+        assert_eq!(xs[1], 6.0);
     }
 }
