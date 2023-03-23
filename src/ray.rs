70bed5d36db7dabe41c949d82cecddea1629f860
Simon Chenery <simon_chenery@yahoo.com>
2023-03-23 20:40:58 +0100

    Add Ray.transform

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use crate::tuple::*;
+use crate::matrix::*;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Ray
@@ -22,6 +23,12 @@ impl Ray
         create_point(ov[0] + t * dv[0],
             ov[1] + t * dv[1],
             ov[2] + t * dv[2])
+    }
+
+    pub fn transform(&self, m: Matrix) -> Ray
+    {
+        Ray{origin: m.multiply_tuple(self.origin),
+            direction: m.multiply_tuple(self.direction)}
     }
 }
 
@@ -62,5 +69,19 @@ mod tests
         assert_eq!(r2.position(1.0), create_point(3.0, 3.0, 4.0));
         assert_eq!(r2.position(-1.0), create_point(1.0, 3.0, 4.0));
         assert_eq!(r2.position(2.5), create_point(4.5, 3.0, 4.0));
+
+        // p.69 Scenario: Translating a ray
+        let r3 = Ray::new(create_point(1.0, 2.0, 3.0), create_vector(0.0, 1.0, 0.0));
+        let m3 = Matrix::translation(3.0, 4.0, 5.0);
+        let r3t = r3.transform(m3);
+        assert_eq!(r3t.origin, create_point(4.0, 6.0, 8.0));
+        assert_eq!(r3t.direction, create_vector(0.0, 1.0, 0.0));
+
+        // p.69 Scenario: Scaling a ray
+        let r4 = Ray::new(create_point(1.0, 2.0, 3.0), create_vector(0.0, 1.0, 0.0));
+        let m4 = Matrix::scaling(2.0, 3.0, 4.0);
+        let r4t = r4.transform(m4);
+        assert_eq!(r4t.origin, create_point(2.0, 6.0, 12.0));
+        assert_eq!(r4t.direction, create_vector(0.0, 3.0, 0.0));
     }
 }
