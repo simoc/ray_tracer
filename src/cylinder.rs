e100cfb729f96372f7f688a4f489924518ec47ab
Simon Chenery <simon_chenery@yahoo.com>
2023-05-07 12:01:34 +0200

    Add Cylinder.local_intersect

--- src/cylinder.rs
+++ src/cylinder.rs
@@ -15,6 +15,7 @@ pub struct Cylinder
 {
 }
 
+// A cylinder following the y axis
 impl Cylinder
 {
     pub fn new() -> Self
@@ -44,9 +45,9 @@ impl Cylinder
             return vec![];
         }
 
-        // this is just a placeholder, to ensure the tests
-        // passs that expect the ray to miss.
-        return vec![1.0];
+        let t0 = ((-b) - disc.sqrt()) / (2.0 * a);
+        let t1 = ((-b) + disc.sqrt()) / (2.0 * a);
+        return vec![t0, t1];
     }
 
     pub fn local_normal_at(&self, point: Tuple) -> Tuple
@@ -85,6 +86,30 @@ mod tests
             let r1 = Ray::new(origins1[i], directions1[i]);
             let xs1 = c1.local_intersect(r1);
             assert_eq!(xs1.len(), 0);
+        }
+    }
+
+    #[test]
+    fn test_cylinders_feature2()
+    {
+        // p.180 Scenario: A ray strikes a cylinder
+        let c2 = Cylinder::new();
+        let origins2 = vec![create_point(1.0, 0.0, -5.0),
+            create_point(0.0, 0.0, -5.0),
+            create_point(0.5, 0.0, -5.0)];
+        let directions2 = vec![create_vector(0.0, 0.0, 1.0),
+            create_vector(0.0, 0.0, 1.0),
+            create_vector(0.1, 1.0, 1.0)];
+        let t20 = vec![5.0, 4.0, 6.80798];
+        let t21 = vec![5.0, 6.0, 7.08872];
+
+        for i in 0..origins2.len()
+        {
+            let r2 = Ray::new(origins2[i], directions2[i].normalize());
+            let xs2 = c2.local_intersect(r2);
+            assert_eq!(xs2.len(), 2);
+            assert!(fuzzy_equal(xs2[0], t20[i]));
+            assert!(fuzzy_equal(xs2[1], t21[i]));
         }
     }
 }
