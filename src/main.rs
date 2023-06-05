0a38f159a8344531d270a3951e757ef693037537
Simon Chenery <simon_chenery@yahoo.com>
2023-06-05 23:03:32 +0200

    Add four pyramids from Chapter 15 to world

--- src/main.rs
+++ src/main.rs
@@ -79,8 +79,53 @@ fn hexagon(id: i32) -> Shape
     material.color = create_color(0.8, 0.1, 0.8);
     material.diffuse = 0.7;
     material.specular = 0.3;
+    material.reflective = 0.6;
     hex.set_material(material);
     return hex;
+}
+
+fn pyramid(id: i32) -> Shape
+{
+    let mut pyramid = Shape::new_group(id);
+    let mut material = Material::new();
+    material.color = create_color(0.1, 0.1, 5.0);
+    material.diffuse = 0.7;
+    material.specular = 0.3;
+    material.shininess = 100.0;
+    material.pattern = None;
+    material.reflective = 0.9;
+    material.transparency = 0.0;
+
+    let size = 0.5;
+    let p1 = create_point(-size, 0.0, -size);
+    let p2 = create_point(-size, 0.0, size);
+    let p3 = create_point(size, 0.0, size);
+    let p4 = create_point(size, 0.0, -size);
+    let p5 = create_point(0.0, size, 0.0);
+
+    let mut triangle1 = Shape::new_triangle(id + 1, p1, p2, p4);
+    triangle1.set_material(material.clone());
+    let mut triangle2 = Shape::new_triangle(id + 2, p2, p3, p4);
+    triangle2.set_material(material.clone());
+
+    let mut triangle3 = Shape::new_triangle(id + 3, p1, p2, p5);
+    triangle3.set_material(material.clone());
+    let mut triangle4 = Shape::new_triangle(id + 4, p2, p3, p5);
+    triangle4.set_material(material.clone());
+    let mut triangle5 = Shape::new_triangle(id + 5, p3, p4, p5);
+    triangle5.set_material(material.clone());
+    let mut triangle6 = Shape::new_triangle(id + 6, p4, p1, p5);
+    triangle6.set_material(material.clone());
+
+    pyramid.add_child(&mut triangle1);
+    pyramid.add_child(&mut triangle2);
+    pyramid.add_child(&mut triangle3);
+    pyramid.add_child(&mut triangle4);
+    pyramid.add_child(&mut triangle5);
+    pyramid.add_child(&mut triangle6);
+    pyramid.set_material(material.clone());
+
+    return pyramid;
 }
 
 fn main()
@@ -167,13 +212,23 @@ fn main()
     let mut hex2 = hexagon(99);
     hex2.set_transform(Matrix::translation(-5.4, 1.0, 2.0).multiply(&Matrix::rotation_x(-PI / 4.0)));
 
+    let mut pyramid1 = pyramid(333);
+    pyramid1.set_transform(Matrix::translation(3.0, 0.0, -1.0));
+    let mut pyramid2 = pyramid(444);
+    pyramid2.set_transform(Matrix::translation(1.0, 0.0, -2.0));
+    let mut pyramid3 = pyramid(555);
+    pyramid3.set_transform(Matrix::translation(-1.0, 0.0, -2.0));
+    let mut pyramid4 = pyramid(666);
+    pyramid4.set_transform(Matrix::translation(-3.0, 0.0, -1.0));
+
     // The light source is white, shining from above and to the left:
     let mut world = World::default_world();
     world.light = PointLight::new(create_point(-10.0, 10.0, -10.0), create_color(1.0, 1.0, 1.0));
     world.objects = vec![floor,
         middle_sphere, right_sphere, left_sphere,
         cube, cylinder,
-        hex1, hex2];
+        hex1, hex2,
+        pyramid1, pyramid2, pyramid3, pyramid4];
 
     // And the camera is configured like so:
     let mut camera = Camera::new(100, 50, PI / 2.0);
